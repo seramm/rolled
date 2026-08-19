fbfeb786dc503cedb087cccd7cec376745f3fb56
seramm <seram@tuta.com>
2026-08-19 18:32:26 +0200

    feat(backend): register models in Django admin dasboard

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,1 +1,10 @@
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import Camera, CameraModel, FilmStock, Roll, User
+
+admin.site.register(User, UserAdmin)
+admin.site.register(CameraModel)
+admin.site.register(Camera)
+admin.site.register(FilmStock)
+admin.site.register(Roll)
